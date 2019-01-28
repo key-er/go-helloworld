4c1045de233489dbe308a2b9cdb22472f58d2018
Nasa Quba <nasaquba@gmail.com>
2019-01-27 19:30:57 -0800

    removed cncrypted creds

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "deploy script running.."
-echo "$DOCKER_USERNAME" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
